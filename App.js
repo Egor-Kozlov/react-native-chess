8f0515bf79d3015183eca008717753e4965083e0
Egor-Kozlov <kozlov1998.2010@gmail.com>
2022-02-20 22:43:18 +0300

    feat: add asyncStorage for boardTheme

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 import Navigate from "./Navigate";
 import BoardThemeContext from "./src/context";
 
+const STORAGE_KEY = "boardTheme";
+
 export default function App() {
   const [pickedTheme, setPickedTheme] = useState("Bases");
+
+  const { getItem, setItem } = useAsyncStorage(STORAGE_KEY);
+
+  const readItemFromStorage = async () => {
+    const jsonValue = await getItem();
+    jsonValue != null ? setPickedTheme(JSON.parse(jsonValue)) : null;
+  };
+
+  const writeItemToStorage = async (newValue) => {
+    const jsonValue = JSON.stringify(newValue);
+    await setItem(jsonValue);
+  };
+
+  useEffect(() => {
+    readItemFromStorage();
+  }, []);
+
+  useEffect(() => {
+    writeItemToStorage(pickedTheme);
+  }, [pickedTheme]);
 
   return (
     <BoardThemeContext.Provider value={{ pickedTheme, setPickedTheme }}>
